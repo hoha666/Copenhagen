47936d86f39390ebb49f472e6bd15d09f7235f66
Habil Harati <habil.harati@outlook.com>
2022-01-15 13:45:39 +0330

    Adding create-react-app template

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import HelloWorldButton from "./components/hello-world-button/hello-world-button.js";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const helloWorldButton = new HelloWorldButton();
-helloWorldButton.render();
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-if (process.env.NODE_ENV === "production") {
-  console.log("Production Mode");
-} else if (process.env.NODE_ENV === "development") {
-  console.log("Development Mode");
-}
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
